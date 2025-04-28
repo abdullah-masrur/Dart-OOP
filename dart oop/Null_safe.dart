3399bb118828f28f02f3b4e0926a48111aaa4281
abdullah-masrur <almasrur.info@gmail.com>
2025-04-28 21:26:17 +0600

    Add files via upload

---
+++ dart oop/Null_safe.dart
@@ -0,0 +1,12 @@
+void main(){
+  int? age;
+  print(age);
+  // The variable 'age' must be initialized before it can be used.
+  //add question mark to make it nullable
+
+
+  String? name;
+  print(name);
+  // The variable 'name' must be initialized before it can be used.
+  //add question mark to make it nullable
+}
